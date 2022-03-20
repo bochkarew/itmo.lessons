d7f604a33b7c80006478cc6ea572d77b80056b29
bochkarew <101887215+bochkarew@users.noreply.github.com>
2022-03-21 01:19:36 +0300

    Update helloWorld.java

--- src/itmo/lessons/lesson8/helloWorld.java
+++ src/itmo/lessons/lesson8/helloWorld.java
@@ -3,6 +3,7 @@ package itmo.lessons.lesson8;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hllo");
+        System.out.println("hellow2");
     }
     
 }
